1b28b8fb317c49db6392b995ba7cbc86705fc232
gengjiawen <technicalcute@gmail.com>
2016-08-18 23:21:00 +0800

    remove on error

--- migrate_android_studio.py
+++ migrate_android_studio.py
@@ -13,26 +13,6 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copy2(s, d)
 
 
-def onerror(func, path, exc_info):
-    """
-    Error handler for ``shutil.rmtree``.
-
-    If the error is due to an access error (read only file)
-    it attempts to add write permission and then retries.
-
-    If the error is for another reason it re-raises the error.
-
-    Usage : ``shutil.rmtree(path, onerror=onerror)``
-    """
-    import stat
-    if not os.access(path, os.W_OK):
-        # Is the error an access error ?
-        os.chmod(path, stat.S_IWUSR)
-        func(path)
-    else:
-        raise
-
-
 def get_immdiate_dir(folder):
     file_list = [os.path.join(folder, i) for i in next(os.walk(folder))[1]]
     return file_list
@@ -51,7 +31,7 @@ def start_migrate(eclipse_project_path, output_path):
         os.makedirs(i, exist_ok=True)
     if os.path.exists(output_path):
         print("remove existing")
-        shutil.rmtree(output_path, onerror=onerror)
+        shutil.rmtree(output_path)
     shutil.copytree(dir_prefix, output_path)
 
     print("handling libs...")
